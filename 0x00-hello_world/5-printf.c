88070f0ae2e097e6b715c22e0f85cba57d8822b7
haile <hailedereje88@gmail.com>
2022-03-11 01:16:27 +0300

    solomon

---
+++ 0x00-hello_world/5-printf.c
@@ -0,0 +1,12 @@
+#!/bin/bash
+#include <stdio.h>
+/**
+ * main - entery point
+ *
+ * Return: 0 after printing the function
+ */
+int main(void)
+{
+	printf("with proper grammar, but the outcome is a piece of art,");
+	return (0);
+}
