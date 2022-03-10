3459c5a7c693770cbcf8c027afd897c2b8429183
haile <hailedereje88@gmail.com>
2022-03-11 01:54:40 +0300

    haile

--- 0x00-hello_world/6-size.c
+++ 0x00-hello_world/6-size.c
@@ -6,10 +6,10 @@
  */
 int main(void)
 {
-	printf("Size of a char: 1 byte(s)");
-	printf("Size of a int: 4 byte(s)");
-	printf("Size of a long int: 4 byte(s)");
-	printf("Size of a long long int: 8 byte(s)");
-	printf("Size of a float: 4 byte(s)");
+	printf("Size of a char: 1 byte(s)\n");
+	printf("Size of a int: 4 byte(s)\n");
+	printf("Size of a long int: 4 byte(s)\n");
+	printf("Size of a long long int: 8 byte(s)\n");
+	printf("Size of a float: 4 byte(s)\n");
 	return (0);
 }
