6df69ac6761008d86d972b975d01354aa98c5b78
haile <hailedereje88@gmail.com>
2022-03-11 23:13:06 +0300

    haile

--- 0x01-variables_if_else_while/4-print_alphabt.c
+++ 0x01-variables_if_else_while/4-print_alphabt.c
@@ -8,9 +8,13 @@ int main(void)
 {
 	int i, x = 97;
 
-	for (i = 0; i <= 25; i++){
+	for (i = 0; i <= 25; i++)
+	{
 		if (putchar(x + i) != 'q' && putchar(x+i) != 'e')
-			putchar(x+i)
+		{
+			putchar(x+i);
+		}
+	}
 	putchar(10);
 	return (0);
 }
