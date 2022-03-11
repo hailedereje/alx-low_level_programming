3d3cfe63608165e8f69b8bcf8f35211427f64b42
haile <hailedereje88@gmail.com>
2022-03-12 00:55:43 +0300

    multiple

--- 0x01-variables_if_else_while/101-print_comb4.c
+++ 0x01-variables_if_else_while/101-print_comb4.c
@@ -11,11 +11,12 @@ int main(void)
 	for (i = 0; i <= 7; i++)
 	{
 		for (j = i + 1; j <= 8; j++){
-			for (y = 0; y <= 9)
+			for (y = j + 1; y <= 9; y++)
 			{
 				
 				putchar(x + i);
 				putchar(x + j);
+				putchar(x + y)
 				if (i != 7 || j != 8 || y != 9)
 				{
 					putchar(44);
