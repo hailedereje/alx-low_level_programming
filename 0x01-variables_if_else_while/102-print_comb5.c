48a90a96e00b14c0f69cefae0ddb3df9e0bd062b
haile <hailedereje88@gmail.com>
2022-03-12 01:14:39 +0300

    haile

---
+++ 0x01-variables_if_else_while/102-print_comb5.c
@@ -0,0 +1,38 @@
+#include <stdio.h>
+/**
+ * main - entery point
+ *
+ * Return: 0 after the function excute
+ */
+int main(void)
+{
+	int y, i = 0, x = 48, j = 0;
+
+	for (i = 0; i <= 9; i++)
+	{
+		for (j = 0; j <= 8; j++)
+		{
+			putchar(x + i);
+			putchar(x + j);
+			if (i != 9 || j != 9)
+			{
+				putchar(32);
+			}
+			for (k = 0; k <=9; k++){
+				for (l = 0; l <=9; l++)
+				{
+					putchar(x+k);
+					putchar(x+l);
+					if (k != 9 || l != 9)
+					{
+						putchar(44);
+					}
+				}
+			}
+
+			
+		}
+	}
+	putchar(10);
+	return (0);
+}
