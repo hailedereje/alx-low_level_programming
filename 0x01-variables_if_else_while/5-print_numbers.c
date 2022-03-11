9eb5878d7240d79e75e812a24e7acf1cc753c255
haile <hailedereje88@gmail.com>
2022-03-11 23:31:31 +0300

    kibrete dorm

---
+++ 0x01-variables_if_else_while/5-print_numbers.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+/**
+ * main - entery point
+ *
+ * Return: 0 after the function excute
+ */
+int main(void)
+{
+	int i;
+
+	for (i = 0; i <= 9; i++)
+	{
+		printf(i);
+	}
+	putchar(10);
+	return (0);
+}
