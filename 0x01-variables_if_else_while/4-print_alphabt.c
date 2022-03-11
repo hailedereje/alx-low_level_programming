bc9ebf2a5d7c83774a8f32a13e3c70ee3aaadac5
haile <hailedereje88@gmail.com>
2022-03-11 23:11:07 +0300

    haile

---
+++ 0x01-variables_if_else_while/4-print_alphabt.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+/**
+ * main - entery point
+ *
+ * Return: 0 after the function excute
+ */
+int main(void)
+{
+	int i, x = 97;
+
+	for (i = 0; i <= 25; i++){
+		if (putchar(x + i) != 'q' && putchar(x+i) != 'e')
+			putchar(x+i)
+	putchar(10);
+	return (0);
+}
