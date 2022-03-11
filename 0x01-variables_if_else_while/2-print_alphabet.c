6394c249ec14a377b3433dc5831459910e4394c4
haile <hailedereje88@gmail.com>
2022-03-11 22:33:53 +0300

    hxaile

---
+++ 0x01-variables_if_else_while/2-print_alphabet.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+/**
+ * main - entery point
+ *
+ * Return: 0 after the function excute
+ */
+int main(void)
+{
+	int x = 97;
+	for (int i = 0;i <= 25;i++)
+		putchar(x + i);
+	return (0);
+}
