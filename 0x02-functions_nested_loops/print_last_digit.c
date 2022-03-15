edf4e0024c9605e9c91507c4018480518153a996
haile <hailedereje88@gmail.com>
2022-03-15 18:55:27 +0300

    haile

---
+++ 0x02-functions_nested_loops/print_last_digit.c
@@ -0,0 +1,10 @@
+#include "main.h"
+/**
+ * print_last_digit - returns the laast digit of the number
+ * @n - takes integer
+ * Return: the last digit
+ */
+int print_last_digit(int n)
+{
+	return (n % 10);
+}
