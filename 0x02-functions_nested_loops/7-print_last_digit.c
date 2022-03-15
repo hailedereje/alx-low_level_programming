97bedcc536c1c5b665916bb8d63a947d64735e99
haile <hailedereje88@gmail.com>
2022-03-15 19:42:59 +0300

    new commit

--- 0x02-functions_nested_loops/7-print_last_digit.c
+++ 0x02-functions_nested_loops/7-print_last_digit.c
@@ -1,7 +1,7 @@
 #include "main.h"
 /**
  * print_last_digit - returns the laast digit of the number
- * @n - takes integer
+ * @n: takes integer
  * Return: the last digit
  */
 int print_last_digit(int n)
