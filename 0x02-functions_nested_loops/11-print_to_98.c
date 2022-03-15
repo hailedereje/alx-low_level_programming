1e280d4e381885c16fc3a2969244b5c825acf626
haile <hailedereje88@gmail.com>
2022-03-16 00:08:08 +0300

    jaoejk

---
+++ 0x02-functions_nested_loops/11-print_to_98.c
@@ -0,0 +1,36 @@
+#include "main.c"
+/**
+ * print_to_98 - the function
+ * @n: integer
+ * void - no return
+ */
+void print_to_98(int n)
+{
+	int i;
+
+	if (n < 98)
+	{
+		
+		for (i = n; i <= 98; i++)
+		{
+			_putchar(i + '0');
+			_putchar(',');
+			_putchar(' ');
+		}
+		_putchar('\n');
+	}
+	else if (n == 98)
+	{
+		_putchar(n + '0');
+	}
+	else
+	{
+		for (i = n; i >= 98; i--)
+		{
+			_putchar(i + '0');
+			_putchar(',');
+			_putchar(' ');
+		}
+		_putchar('\n');
+	}
+}
