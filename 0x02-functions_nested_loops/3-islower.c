cc4ad83ce9a46bfebefacc5b84b9476de357dcb9
haile <hailedereje88@gmail.com>
2022-03-15 18:03:17 +0300

    commit

---
+++ 0x02-functions_nested_loops/3-islower.c
@@ -0,0 +1,17 @@
+#include "main.h"
+/**
+ * Return: 1 if it is lowercase
+ *
+ * else: 0
+ */
+int islower(int c)
+{
+	if (c >= 'a' && c <= 'z')
+	{
+		return (1);
+	}
+	else
+	{
+		return (0);
+	}
+}
